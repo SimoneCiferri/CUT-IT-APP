cbdeda04691c4ec69361f6adcc1298d3de300f4d
SimoneCiferri <73128699+SimoneCiferri@users.noreply.github.com>
2021-01-26 00:35:55 +0100

    LoginView

---
+++ src/codice/MainView.java
@@ -0,0 +1,60 @@
+package codice;
+
+import java.io.IOException;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+	
+	private boolean Running=false;
+	private static Stage prStage;
+	private static BorderPane prLayout;
+	private BorderPane homeLayout = null;
+	private BorderPane signupLayout = null;
+	private BorderPane statusBar = null;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		setMainLayout(stage);
+		prStage.setTitle("CUT-IT");
+		
+		startPrLayout();
+		
+		Scene scene = new Scene(prLayout);
+		prStage.setScene(scene);
+		prStage.show();
+		prStage.setResizable(false);
+		
+	}
+	
+	public static boolean startPrLayout() throws IOException {
+		prLayout = FXMLLoader.load(MainView.class.getResource("login.fxml"));
+		return true;
+	}
+	
+	public static void setMainLayout(Stage stage1) {
+		prStage=stage1;
+	}
+	
+	public static BorderPane getPrLayout() {
+		return prLayout;
+	}
+	
+	public static Stage getPrWindow() {
+		return prStage;
+	}
+	
+	public boolean isRunning() {
+		return Running;
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
--- src/codice/View.java
+++
@@ -1,6 +0,0 @@
-package codice;
-
-public abstract class View {
-	
-	public abstract void update();
-}
