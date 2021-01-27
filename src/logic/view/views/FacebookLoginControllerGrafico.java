00c145bbd1fe764f9bea9bd7cf5e384098a0c0cd
SimoneCiferri <73128699+SimoneCiferri@users.noreply.github.com>
2021-01-27 23:50:10 +0100

    controller grafici

---
+++ src/logic/view/views/FacebookLoginControllerGrafico.java
@@ -0,0 +1,9 @@
+package logic.view.views;
+
+import javafx.scene.layout.BorderPane;
+import logic.view.MainView;
+
+public class FacebookLoginControllerGrafico {
+	
+	private BorderPane mainLayout = MainView.getPrLayout();
+}
---
+++ src/logic/view/views/bookAppointmentControllerGrafico.java
@@ -0,0 +1,10 @@
+package logic.view.views;
+
+import javafx.scene.layout.BorderPane;
+import logic.view.MainView;
+
+public class bookAppointmentControllerGrafico {
+	
+	private BorderPane mainLayout = MainView.getPrLayout();
+	
+}
